8143858d070a5d96a78f20c2a7d2cbf562b6faa8
Victor Moraes <victor.moraes@outlook.com.br>
2026-05-26 22:37:27 -0300

    Format ModificarCargoDto and ReajusteSalarioDto for improved readability by adding line breaks between fields

--- src/main/java/com/webservice/cadastro_funcionarios/dtos/ModificarCargoDto.java
+++ src/main/java/com/webservice/cadastro_funcionarios/dtos/ModificarCargoDto.java
@@ -3,5 +3,8 @@ package com.webservice.cadastro_funcionarios.dtos;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
-public record ModificarCargoDto(@NotNull UUID FuncionarioId,
-                                @NotNull String Cargo) { }
+public record ModificarCargoDto(@NotNull
+                                UUID FuncionarioId,
+
+                                @NotNull
+                                String Cargo) { }
--- src/main/java/com/webservice/cadastro_funcionarios/dtos/ReajusteSalarioDto.java
+++ src/main/java/com/webservice/cadastro_funcionarios/dtos/ReajusteSalarioDto.java
@@ -4,5 +4,9 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
-public record ReajusteSalarioDto(@NotNull UUID FuncionarioId,
-                                 @NotNull @DecimalMin(value = "1.0", message = "O ajuste deve ser superior a 1%") Float PercentualAjuste) { }
+public record ReajusteSalarioDto(@NotNull
+                                 UUID FuncionarioId,
+
+                                 @NotNull
+                                 @DecimalMin(value = "1.0", message = "O ajuste deve ser superior a 1%")
+                                 Float PercentualAjuste) { }
